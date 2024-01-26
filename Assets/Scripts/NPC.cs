df29c8c96a1c177cce5cf885692b3e982becda16
CristopherNicolas <cristopher.faundez@uniacc.edu>
2024-01-26 19:29:10 -0300

    0.6

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 // los npc salen hasta el escenario
+[RequireComponent(typeof(NavMeshAgent))]
 public abstract class NPC : MonoBehaviour
 {
    
