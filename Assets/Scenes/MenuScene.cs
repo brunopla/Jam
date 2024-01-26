5c4cdb789bd5637a265f532609251c703889df89
brunopla <102322121+brunopla@users.noreply.github.com>
2024-01-26 18:37:09 -0300

    menuupdate

--- Assets/Scenes/MenuScene.cs
+++ Assets/Scenes/MenuScene.cs
@@ -6,10 +6,30 @@ using UnityEngine.SceneManagement; //libreria para usar funciones de sceneManage
 
 public class MenuScene : MonoBehaviour
 {
+    public GameObject menu;  // Asegúrate de asignar tu objeto de menú en el inspector de Unity
+    private string escenaActual;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !menu.activeSelf)
+        {
+            escenaActual = SceneManager.GetActiveScene().name;
+            menu.SetActive(true);
+            SceneManager.LoadScene("menu principal");
+        }
+    }
+
     public void JugarJuego()
     {
-        SceneManager.LoadScene("Escena principal");  
+        SceneManager.LoadScene("Escena principal");
     }
+
+    public void Volver()
+    {
+        menu.SetActive(false);
+        SceneManager.LoadScene(escenaActual);
+    }
+
     public void SalirJuego()
     {
         Application.Quit();
