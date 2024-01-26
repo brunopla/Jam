9b8c50dfafa635195e99eb3651e7ac544d1b7b93
romanleco <94546246+romanleco@users.noreply.github.com>
2024-01-26 19:03:01 -0300

    Player Movimiento Basico

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _cc;
+    private Vector3 _inputVector;
+    [SerializeField] private float _velocidad;
+    [SerializeField] private float _velocidadRotacion;
+
+    void Start()
+    {
+        _cc = GetComponent<CharacterController>();
+        if(_cc == null)
+        {
+            Debug.LogError("Character Controller NULL");
+        }
+    }
+
+    void Update()
+    {
+        _inputVector.x = Input.GetAxis("Horizontal");
+        _inputVector.z = Input.GetAxis("Vertical");
+
+        _cc.Move(_inputVector * _velocidad * Time.deltaTime);
+
+        if(_inputVector != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(_inputVector, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, _velocidadRotacion * Time.deltaTime);
+        }
+    }
+}
