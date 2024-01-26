8f885c34af0e5cc0951e2f966fa3e62d8301646d
CristopherNicolas <cristopher.faundez@uniacc.edu>
2024-01-26 17:13:25 -0300

    v0.2

---
+++ Assets/GameLoop.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoop : MonoBehaviour
+{
+   // sistema publico 
+   // public GameObject Player
+}
