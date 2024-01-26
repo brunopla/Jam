87b84985cd4564ae3094225bf501b51b173f6ba5
romanleco <94546246+romanleco@users.noreply.github.com>
2024-01-26 19:59:04 -0300

    ItemBase

---
+++ Assets/Scripts/ItemBase.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemBase : MonoBehaviour
+{
+    // public abstract void Interaccion()
+    // {
+    //     Debug.Log("Interaccion");
+    // }
+
+    // void OnTriggerEnter(Collider other);
+    // {
+    //     if(other.transform.tag == "Player")
+    //     {
+    //         Interaccion();
+    //     }
+    // }
+}
